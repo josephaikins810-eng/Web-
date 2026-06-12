f36af9467d80ae59d2735c1df55696006b7c5ca4
josephaikins810-eng <josephaikins810@gmail.com>
2026-06-12 22:50:20 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+﻿$(function() {
+    $('.hamburger').on('click', function() {
+        $('.nav-links').toggleClass('active');
+    });
+
+    $('.nav-links a').on('click', function() {
+        $('.nav-links').removeClass('active');
+    });
+
+    if ($('.search-input').length) {
+        $('.search-input').on('keyup', function() {
+            var value = $(this).val().toLowerCase();
+            $('.card').filter(function() {
+                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+            });
+        });
+    }
+
+    var visits = Number(localStorage.getItem('visits') || 0) + 1;
+    localStorage.setItem('visits', visits);
+    $('#visitor-count').text(visits);
+});
